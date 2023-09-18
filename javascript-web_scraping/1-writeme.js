a61b91d8dba5e37ffb088d3117affed645c5d9ad
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-09-18 23:09:48 +0200

    Add: task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+const string = process.argv[3];
+
+fs.writeFile(filePath, string, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(data);
   }
 });
