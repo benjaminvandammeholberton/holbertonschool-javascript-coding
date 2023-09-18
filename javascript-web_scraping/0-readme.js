a4792695a9277e1f8d1d8a177e45df10f7dac63a
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-09-18 19:19:10 +0200

    Fix: task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -6,5 +6,7 @@ const filePath = process.argv[2];
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-  } else console.log(data);
+  } else {
+    console.log(data);
+  }
 });
