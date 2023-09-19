b9b9d88d506b8f11bb0536b4200ad485703e6c6c
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-09-19 14:17:39 +0200

    Fix: semistandard

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,24 +4,20 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-        console.error(error);
-    } else {
-        const data = JSON.parse(body)
-        const new_dict = {}
-        for (item of data) {
-            if (item.completed === true) {
-                if (!new_dict[item.userId]){
-                    new_dict[item.userId] = 1
-                } else {
-                    new_dict[item.userId] = new_dict[item.userId] + 1
-                }
-                
-            }
-        
-            
+  if (error || response.statusCode !== 200) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    const new_dict = {};
+    for (item of data) {
+      if (item.completed === true) {
+        if (!new_dict[item.userId]) {
+          new_dict[item.userId] = 1;
+        } else {
+          new_dict[item.userId] = new_dict[item.userId] + 1;
         }
-        console.log(new_dict)
-        
+      }
     }
-})
+    console.log(new_dict);
+  }
+});
