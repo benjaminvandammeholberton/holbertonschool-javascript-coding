681ddff4f598a0e97d4cd5882f0e64b75b8af483
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-09-19 13:49:06 +0200

    Add: task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+const request = require ('request');
+const url = process.argv[2];
+const fs = require ('fs');
+const filePath = process.argv[3];
+
+
+request.get(url, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+        console.error(error);
+    } else {
+        fs.writeFile(filePath, body, 'utf-8', (err) => {
+            if (err) {
+                console.error(err)
+            }
+        })
+}})
