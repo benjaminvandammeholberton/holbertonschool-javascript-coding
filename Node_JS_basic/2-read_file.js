743b5cd2a729575ad78f16058116cf9b8ddc1550
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-06 18:11:15 +0200

    Fix: task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,35 +1,48 @@
 const fs = require('fs');
 
-function countStudents(path) {
+function countStudents(filePath) {
   try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.split('\n');
+    const data = fs.readFileSync(filePath, 'utf8');
+    const raws = data.split('\n');
+    const parsedData = [];
 
-    const students = lines.slice(1).map((line) => {
-      const [firstName, lastName, age, field] = line.split(',');
+    for (let i = 1; i < raws.length; i += 1) {
+      const row = raws[i].trim();
+      const columns = row.split(',');
 
-      return {
-        firstName,
-        lastName,
-        age: parseInt(age, 10),
-        field,
+      const student = {
+        firstname: columns[0].trim(),
+        lastname: columns[1].trim(),
+        age: columns[2].trim(),
+        field: columns[3].trim(),
       };
-    });
+      parsedData.push(student);
+    }
+    const numberOfStudents = parsedData.length;
+    console.log(`Number of students: ${numberOfStudents}`);
 
-    const csStudents = students.filter((student) => student.field === 'CS');
-    const sweStudents = students.filter((student) => student.field === 'SWE');
+    const fieldsName = [];
 
-    console.log(`Number of students: ${students.length}`);
-    console.log(
-      `Number of students in CS: ${csStudents.length}. List: ${csStudents
-        .map((student) => student.firstName)
-        .join(', ')}`
-    );
-    console.log(
-      `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents
-        .map((student) => student.firstName)
-        .join(', ')}`
-    );
+    for (let i = 0; i < parsedData.length; i += 1) {
+      const student = parsedData[i];
+      if (!fieldsName.includes(student.field)) {
+        fieldsName.push(student.field);
+      }
+    }
+
+    const fieldDictionary = {};
+    for (let i = 0; i < fieldsName.length; i += 1) {
+      const fieldName = fieldsName[i];
+      const listStudent = parsedData
+        .filter((student) => student.field === fieldName)
+        .map((student) => student.firstname);
+      fieldDictionary[fieldName] = listStudent;
+      console.log(
+        `Number of students in ${fieldName}: ${
+          listStudent.length
+        }. List: ${listStudent.join(', ')}`
+      );
+    }
   } catch (err) {
     throw new Error('Cannot load the database');
   }
