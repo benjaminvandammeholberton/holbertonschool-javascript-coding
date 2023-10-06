298ff58f55c07cb8002be206d670d604fa156bb0
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-06 22:38:22 +0200

    Fix: task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,21 +2,24 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
+  res.write('This is the list of our students\n');
   try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`This is the list of our students\n${data.join('\n')}`);
+    const students = await countStudents(process.argv[2]);
+    res.end(students.join('\n'));
+    res.statusCode = 200;
+    res.end();
   } catch (error) {
-    res.send(error.message);
+    res.statusCode = 500;
+    res.end(error.message);
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(1245);
+
+module.exports = app;
