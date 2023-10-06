a7f0c1b91199f5d0cde4e86419ed3f5f0ddadb41
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-06 14:50:04 +0200

    Fix: task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
+  const input = data.toString().trim();
+  process.stdout.write(`Your name is: ${input}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
