ecfd429afddc8a5d2930ec4711fa22c61e16bc1f
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-10-13 22:14:34 +0200

    Add: task 8

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,7 +1,5 @@
-class AppController {
-  getHomepage(request, response) {
+export default class AppController {
+  static getHomepage(request, response) {
     response.status(200).send('Hello Holberton School!');
   }
 }
-
-module.exports = AppController;
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,18 +1,32 @@
+// full_server/utils.js
+
 const fs = require('fs');
+
 function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
       } else {
-        const messages = [];
-        let message;
-        const content = data.toString().split('\n');
-        console.log(content);
+        const lines = data.trim().split('\n');
+        lines.shift(); // Supprimer l'en-tête
 
-        resolve(messages);
+        const studentsByField = {};
+
+        for (const line of lines) {
+          const [firstname, , , field] = line.split(',');
+
+          if (!studentsByField[field]) {
+            studentsByField[field] = [firstname];
+          } else {
+            studentsByField[field].push(firstname);
+          }
+        }
+
+        resolve(studentsByField);
       }
     });
   });
 }
-readDatabase('./database.csv');
+
+export default readDatabase;
